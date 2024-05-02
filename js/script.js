82ec0417c4c74f0fe78eb85ad6bdb5ea852a653a
datadham <adham1901@hotmail.com>
2024-05-03 00:13:33 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function runTypingEffect() {
-  const text = 'I am Brad Traversy.';
+  const text = 'I am Adham.';
   const typingElement = document.getElementById('typing-text');
   const typingDelay = 100;
 
